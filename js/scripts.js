61738c42595e3734f5c4a9be53609ad927d81776
Lawrence Kwok <lawrence.kwok@me.com>
2023-02-27 17:07:17 -0600

    Update ID and class names to be more consistent

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ const luggageRows = new Map([ [1,'Luggage 1'] ]);
 let luggageIDIncrement = 1;
 
 function calculateWeights() {
-	const bodyWeightInput = document.getElementById('bodyWeight').value;
-	const weightHTML = ' <span class="weightUnit">lb</span>';
+	const bodyWeightInput = document.getElementById('body-weight').value;
+	const weightHTML = ' <span class="weight-unit">lb</span>';
 	let totalWiggleRoom = 0;
 
 	if (!bodyWeightInput) {
@@ -11,9 +11,9 @@ function calculateWeights() {
 	};
 	
 	for (let [id, value] of luggageRows) {
-		let scaleWeightInput = document.getElementById('scaleWeight-'+id).value;
-		let luggageWeightOutput = document.getElementById('luggageWeight-'+id)
-		let wiggleRoomOutput = document.getElementById('wiggleRoom-'+id);
+		let scaleWeightInput = document.getElementById('scale-weight-'+id).value;
+		let luggageWeightOutput = document.getElementById('luggage-weight-'+id)
+		let wiggleRoomOutput = document.getElementById('wiggle-room-'+id);
 
 		if (!scaleWeightInput || !bodyWeightInput ) {
 			clearCell(luggageWeightOutput);
@@ -30,7 +30,7 @@ function calculateWeights() {
 		let luggageWeight = (scaleWeightInput - bodyWeightInput);
 		luggageWeightOutput.innerHTML = luggageWeight.toFixed(1) + weightHTML;
 
-		let maxWeightInput = document.getElementById('maxWeight').value;
+		let maxWeightInput = document.getElementById('max-weight').value;
 		if (!maxWeightInput) {
 			clearCell(wiggleRoomOutput);
 			continue;
@@ -46,7 +46,7 @@ function calculateWeights() {
 		totalWiggleRoom += wiggleRoom;
 	}
 
-	let heavyMessage = document.getElementById('heavyMessage');
+	let heavyMessage = document.getElementById('heavy-message');
 	if (totalWiggleRoom < 0) {
 		text = Math.abs(totalWiggleRoom).toFixed(1) + weightHTML;
 		heavyMessage.innerHTML = '<p>Something\'s too heavy. After substracting wiggle room, you still need to remove '+text+'.</p>';
@@ -61,25 +61,26 @@ function addLuggage() {
 	const id = luggageIDIncrement;
 
 	if ('content' in document.createElement('template')) {
-    	const tbody = document.querySelector('#luggageList');
-    	const template = document.querySelector('#luggageRow');
+    	const tbody = document.getElementById('luggage-list');
+    	const template = document.getElementById('luggage-row');
 
     	const clone = template.content.cloneNode(true);
     	let tr = clone.querySelectorAll('tr');
 		let td = clone.querySelectorAll('td');
 		
 		tr[0].setAttribute('id','luggage-'+id);
-		td[0].innerHTML = '<button type="button" tabindex="-1" onclick="removeLuggage('+id+')">– </button> &nbsp; Luggage ' + id;
-		td[1].innerHTML = '<input type="number" id="scaleWeight-'+id+'" class="weightInput"> <span class="weightUnit">lb</span>';
-		td[2].innerHTML = '<td><span id="luggageWeight-'+id+'"></span></td>';
-		td[3].innerHTML = '<td><span id="wiggleRoom-'+id+'"></span></td>';
-
+		td[0].innerHTML = '<button type="button" tabindex="-1" onclick="removeLuggage('+id+')">–</button>';
+		td[1].innerHTML = '<span class="luggage-description" contenteditable="true" tabindex="-1">Luggage '+id+'</span>';
+		td[2].innerHTML = '<input type="number" id="scale-weight-'+id+'" class="weight-input"> <span class="weight-unit">lb</span>';
+		td[3].innerHTML = '<td><span id="luggage-weight-'+id+'"></span></td>';
+		td[4].innerHTML = '<td><span id="wiggle-room-'+id+'"></span></td>';
+		
     	tbody.appendChild(clone);
 	} else {
 	  	// Find another way to add the rows to the table because
 	  	// the HTML template element is not supported.
 
-		// const allLuggage = document.getElementById('luggageList');
+		// const allLuggage = document.getElementById('luggage-list');
 		// let html = '<tr>';
 		// html += '<td>Luggage 2</td></tr>';
 		// allLuggage.insertAdjacentHTML('beforeend', html);
@@ -97,7 +98,7 @@ function clearCell(element) { element.innerHTML = ''; }
 
 function toggleUnits(button) {
 	let weightButton = button.innerHTML;
-	let unitDisplay = document.getElementsByClassName('weightUnit');
+	let unitDisplay = document.getElementsByClassName('weight-unit');
 	weightUnit = (weightButton == 'kg' ? 'kg' : 'lb');
 	for (let i of unitDisplay) {
 		i.innerHTML = weightUnit;
@@ -105,9 +106,11 @@ function toggleUnits(button) {
 }
 
 function toggleInstructions(button){
-	let instructions = document.getElementById("instructions");
+	let instructions = document.getElementById('instructions');
 	instructions.style.display = instructions.style.display === 'none' ? '' : 'none';
-	button.textContent = button.textContent === 'Show instructions...' ? 'Hide instructions...' : 'Show instructions...';
+	if (button) {
+		button.textContent = button.textContent === 'Show instructions...' ? 'Hide instructions...' : 'Show instructions...';
+	}
 }
 
-document.getElementById("bodyWeight").focus();
+document.getElementById('body-weight').focus();
