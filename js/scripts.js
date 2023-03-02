2462a9b70998c6119e391375832bdb0ebead77d9
Lawrence Kwok <lawrence.kwok@me.com>
2023-03-02 12:24:45 -0600

    Fixed bugs; inline alerts
    Fix: If bodyweight removed, only first row calculations are cleared
    Fix: Fields don't clear when body weight is empty or scale weight is too low
    Refactor: Move more HTML from scripts to <template>
    Feature: Inline error message when inputs are below min weight
    Feature: Added dashes to empty fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const luggageRows = new Map();
 let luggageIDIncrement = 0;
-const weightHTML = ' <span class="weight-unit">lb</span>';
+const weightHTML = '<span class="weight-unit">lb</span>';
 const bodyWeightInput = document.getElementById('body-weight');
 
 function clearCell(element) {
@@ -20,20 +20,25 @@ function calculateWeights() {
   // } else {
   //   clearCell(bodyWeightAlert);
   // }
+
   // eslint-disable-next-line no-restricted-syntax
   for (const [id] of luggageRows) {
-    const scaleWeightInput = document.getElementById(`scale-weight-${id}`).value;
+    const scaleWeightInput = document.getElementById(`scale-weight-${id}`);
     const luggageWeightOutput = document.getElementById(`luggage-weight-${id}`);
     const wiggleRoomOutput = document.getElementById(`wiggle-room-${id}`);
 
+    // reset luggage row data and errors
     clearCell(luggageWeightOutput);
     clearCell(wiggleRoomOutput);
+    document.getElementById(`alert-${id}`).style.display = 'none';
 
-    // if (scaleWeightInput <= bodyWeightInput) {
-    // alert(`"Scale Weight" for Luggage ${id} should be more than your body weight.\r\rMake sure you weigh yourself and the luggage at the same time.`);
-    // }
-    if (scaleWeightInput) {
-      const luggageWeight = (scaleWeightInput - bodyWeightInput.value);
+    if (scaleWeightInput.value !== '' && (scaleWeightInput.value <= bodyWeightInput.value)) {
+      document.getElementById(`alert-${id}`).style.display = '';
+      continue;
+    }
+
+    if (bodyWeightInput.value !== '' && scaleWeightInput.value !== '') {
+      const luggageWeight = (scaleWeightInput.value - bodyWeightInput.value);
       luggageWeightOutput.innerHTML = luggageWeight.toFixed(1) + weightHTML;
 
       if (maxWeightInput) {
@@ -48,11 +53,8 @@ function calculateWeights() {
         }
         totalWiggleRoom += wiggleRoom;
       }
-    } else {
-      return;
     }
   }
-
   weightMessage(totalWiggleRoom, maxWeightInput, overweightCount);
 }
 
@@ -61,7 +63,7 @@ function weightMessage(totalWiggleRoom, maxWeightInput, overweightCount) {
   const howManyBags = overweightCount > 1 ? 'Some bags are too heavy. ' : 'A bag is too heavy. ';
   if (totalWiggleRoom < 0) {
     const text = Math.abs(totalWiggleRoom).toFixed(1) + weightHTML;
-    heavyMessage.innerHTML = `<span class="overweight">${howManyBags} After accounting for wiggle room, you still need to remove ${text}.</span>`;
+    heavyMessage.innerHTML = `<span class="overweight">${howManyBags} Consider moving items to bags with wiggle room. However, you would still need to remove ${text}.</span>`;
   } else if (totalWiggleRoom >= 0 && overweightCount >= 1) {
     heavyMessage.innerHTML = `<span class="overweight">${howManyBags}</span><span class="underweight">However, you have enough wiggle room in another bag. Consider rearranging your items to stay under the weight limit.</span>`;
   } else if ((maxWeightInput) && (totalWiggleRoom <= 0) || (overweightCount === 0)) {
@@ -75,9 +77,7 @@ function addLuggage() {
   const id = luggageIDIncrement;
 
   const easterEgg = document.getElementById('no-luggage');
-  easterEgg.classList.remove('easter-egg');
-
-  easterEgg.innerHTML = '';
+  easterEgg.style.display = 'none';
 
   if ('content' in document.createElement('template')) {
     const tbody = document.getElementById('luggage-list');
@@ -89,10 +89,13 @@ function addLuggage() {
 
     tr[0].setAttribute('id', `luggage-${id}`);
     td[0].innerHTML = `<button class="remove-button" type="button" tabindex="-1" onclick="removeLuggage(${id})">–</button>`;
+    td[0].setAttribute('id', `button-${id}`);
     td[1].innerHTML = `<span class="luggage-description" contenteditable="true" tabindex="-1" placeholder="Luggage ${id}">Luggage ${id}</span>`;
+    td[1].setAttribute('id', `description-${id}`);
     td[2].innerHTML = `<input type="number" step=".1" id="scale-weight-${id}" class="weight-input has-min" min="${bodyWeightInput.value}"> <span class="weight-unit">lb</span>`;
-    td[3].innerHTML = `<td><span id="luggage-weight-${id}"></span></td>`;
-    td[4].innerHTML = `<td><span id="wiggle-room-${id}"></span></td>`;
+    td[3].setAttribute('id', `luggage-weight-${id}`);
+    td[4].setAttribute('id', `wiggle-room-${id}`);
+    td[5].setAttribute('id', `alert-${id}`);
 
     tbody.appendChild(clone);
   } else {
@@ -116,8 +119,7 @@ function removeLuggage(id) {
   if (luggageRows.size === 0) {
     const easterEgg = document.getElementById('no-luggage');
     luggageIDIncrement = 0;
-    easterEgg.setAttribute('class', 'easter-egg');
-    easterEgg.innerHTML = 'You have no luggage.';
+    easterEgg.style.display = '';
   }
 }
 
